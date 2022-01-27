1251ba81ad323d4fc76233555061da748e03c5d0
willuwontu <willuwontu3@gmail.com>
2022-01-27 09:24:41 -0800

    Update LobbyMonitor.cs

--- LobbyCodes/Networking/LobbyMonitor.cs
+++ LobbyCodes/Networking/LobbyMonitor.cs
@@ -7,15 +7,36 @@ namespace LobbyCodes.Networking
 {
     class LobbyMonitor : MonoBehaviourPunCallbacks
     {
+        public static LobbyMonitor instance {get; private set;}
+
         private void Start()
         {
-
+            instance = this;
         }
 
         public override void OnJoinedRoom()
         {
+            LobbyUI.BG.SetActive(false);
+            // If we're offline, we don't care whether codes are visible or the room we join
             if (!PhotonNetwork.OfflineMode)
             {
+                // If our room is not private, we're entering an unmodded room.
+                if (PhotonNetwork.CurrentRoom.IsVisible)
+                {
+                    // See if we're the master client
+                    if (!PhotonNetwork.LocalPlayer.IsMasterClient)
+                    {
+                        // Leave room if we're not
+                        PhotonNetwork.LeaveRoom();
+                        return;
+                    }
+                    else
+                    {
+                        // Remove room visibility if we are
+                        PhotonNetwork.CurrentRoom.IsVisible = false;
+                    }
+                }
+
                 LobbyUI.BG.SetActive(true);
                 LobbyUI.UpdateLobbyCode(LobbyCodeHandler.GetCode());
                 LobbyCodes.instance.ExecuteAfterSeconds(5f, () => LobbyUI.BG.transform.SetAsLastSibling());
