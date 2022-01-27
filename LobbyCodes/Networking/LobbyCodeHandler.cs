95d0cc29a4e2fdc5a311c85d05f9aa7b9d6fa3f1
pdcook <qphysicscook@gmail.com>
2022-01-26 20:32:20 -0500

    completed merge

---
+++ LobbyCodes/Networking/LobbyCodeHandler.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using LobbyCodes.Utils;
+using UnboundLib;
+using System;
+using System.Text;
+
+namespace LobbyCodes.Networking
+{
+    public static class LobbyCodeHandler
+    {
+        private static string GetPureCode()
+        {
+            if (PhotonNetwork.OfflineMode || PhotonNetwork.CurrentRoom == null) { return ""; }
+            return $"{PhotonNetwork.CloudRegion}:{PhotonNetwork.CurrentRoom.Name}";
+        }
+        public static string GetCode()
+        {
+            return ObfuscateJoinCode.Obfuscate(GetPureCode());
+        }
+        private static void PureConnectToRoom(string pureCode)
+        {
+            string[] reg_room = pureCode.Split(':');
+            string region = reg_room[0];
+            string room = reg_room[1];
+            NetworkConnectionHandler.instance.ForceRegionJoin(region, room);
+        }
+        public static void ConnectToRoom(string obfuscatedCode)
+        {
+            PureConnectToRoom(ObfuscateJoinCode.DeObfuscate(obfuscatedCode));
+        }
+
+    }
+}
---
+++ LobbyCodes/UI/JoinUI.cs
@@ -0,0 +1,41 @@
+﻿using UnboundLib.Utils.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using LobbyCodes.Networking;
+using UnityEngine.SceneManagement;
+using UnboundLib;
+
+namespace LobbyCodes.UI
+{
+    static class JoinUI
+    {
+        static string currentCode = "";
+        static void SetupUI(bool firstTime)
+        {
+            Unbound.Instance.ExecuteAfterSeconds(firstTime ? 0.2f : 0f, () =>
+            {
+                var onlineGo = GameObject.Find("/Game/UI/UI_MainMenu/Canvas/ListSelector/Online");
+                var spaceGo = onlineGo?.transform?.Find("Space")?.transform;
+                var inviteGo = onlineGo?.transform?.Find("Invite friend")?.transform;
+                int siblingIndex = spaceGo != null ? spaceGo.GetSiblingIndex() + 1 : inviteGo != null ? inviteGo.GetSiblingIndex() : 4;
+                var joinMenu = MenuHandler.CreateMenu("JOIN LOBBY", () => { }, onlineGo.gameObject, 60, true, false, null, true, siblingIndex);
+                MenuHandler.CreateText("ENTER LOBBY CODE", joinMenu, out var _);
+                MenuHandler.CreateText(" ", joinMenu, out var _, 30);
+                MenuHandler.CreateInputField("", 60, joinMenu, (string str) => JoinUI.currentCode = str);
+                MenuHandler.CreateButton("JOIN", joinMenu, () => LobbyCodeHandler.ConnectToRoom(currentCode), 60);
+            });
+        }
+
+        internal static void Init()
+        {
+            SceneManager.sceneLoaded += (scene, mode) =>
+            {
+                if (scene.name == "Main")
+                {
+                    SetupUI(false);
+                }
+            };
+            SetupUI(true);
+        }
+    }
+}
